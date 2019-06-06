6c3f92780cbe3e554dcbc13ad9e52c5701686f9c
Johan Tell <johan@mintcore.se>
2019-06-06 17:52:40 +0200

    Create coauthors storage file if it is missing (#1)

--- src/coauthors_file.rs
+++ src/coauthors_file.rs
@@ -2,6 +2,7 @@ use crate::coauthor::Coauthor;
 use serde::{Deserialize, Serialize};
 use shellexpand::tilde;
 use std::fs;
+use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
 struct CoauthorsStorage {
@@ -9,7 +10,14 @@ struct CoauthorsStorage {
 }
 
 fn coauthors_file() -> String {
-    return tilde("~/.config/coauthor/coauthors.toml").to_string();
+    let file_path_string = tilde("~/.config/coauthor/coauthors.toml").to_string();
+    let file_path = Path::new(&file_path_string);
+
+    if !Path::new(&file_path).exists() {
+       fs::File::create(file_path);
+    }
+
+    return file_path_string;
 }
 
 pub fn store_coauthor(coauthor: Coauthor) {
@@ -26,6 +34,11 @@ pub fn store_coauthor(coauthor: Coauthor) {
 
 pub fn read_coauthors() -> Vec<Coauthor> {
     let file_contents = fs::read_to_string(coauthors_file()).expect("Unable to read file");
+
+    if file_contents == "" {
+        return vec![];
+    }
+
     let coauthors_storage: CoauthorsStorage = toml::from_str(&file_contents).unwrap();
 
     return coauthors_storage.coauthors;
